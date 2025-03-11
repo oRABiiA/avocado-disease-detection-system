cdbe2503f773c9a42b63cca3333c731868a6d36f
Rabea Lahham <Rabealahham2@gmail.com>
2025-03-11 23:10:07 +0200

    updated homepage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.logo}>AgriTech</div>
+            <div className={styles.logo}>AgriTech©</div>
             <div className={styles.text}>
                 Avocado Tree Detection System © All rights reserved.
             </div>
